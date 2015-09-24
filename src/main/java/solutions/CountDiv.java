b2656d6a9fbb871b8e8aea48d0f4bc9596e64d60
maiorovi <maiorov.prog@gmail.com>
2015-09-24 21:46:39 +0300

    implemented task CountDiv

--- src/main/java/solutions/CountDiv.java
+++ src/main/java/solutions/CountDiv.java
@@ -3,21 +3,24 @@ package solutions;
 public class CountDiv {
 
     public static void main(String[] args) {
-        System.out.println(new CountDiv().solution(0, 2000000000,1));
+        System.out.println(new CountDiv().solution(0, 2000000000, 1));
     }
 
     public int solution(int A, int B, int K) {
         int counter = 0;
+        int val = 0;
 
         if (A == B) {
               return A %  K == 0 ? 1 : 0;
         }
 
         if (A != 0) {
-            A = A - 1;
+            val = A - 1;
+        } else {
+            val = A;
         }
 
-        counter += B / K -  A / K;
+        counter += B / K -  val / K;
 
         if (A == 0)
             counter++;
