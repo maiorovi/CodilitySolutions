b39cffa44f01ac8a89010a4a50f0c1e6b30afaff
maiorovi <maiorov.prog@gmail.com>
2015-09-24 22:05:50 +0300

    implemented task CountDiv

--- src/main/java/solutions/PassingCars.java
+++ src/main/java/solutions/PassingCars.java
@@ -1,4 +1,19 @@
 package solutions;
 
 public class PassingCars {
+
+    public int solution(int[] A) {
+        int count = 0;
+        int count_zeros = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0)
+                count_zeros++;
+            if (A[i] == 1)
+                count += count_zeros * 1;
+        }
+
+        if (count < 0 || count > 1000000000)
+            return -1;
+        return count;
+    }
 }
